678115cbafbcb349fc09a72244db38d0ac8c534d
Anh3h <courageangeh@gmail.com>
2018-06-07 19:00:04 +0100

    Containerized Customer, deposit-account manager and provisioner

--- migration_script.sh
+++ migration_script.sh
@@ -1,34 +1,16 @@
 declare -a domain_ms=("provisioner" "identity" "rhythm" "office" "customer" "portfolio" "deposit-account-management" "teller" "reporting" "payroll")
-declare -a core_ms=("api" "async" "cassandra" "command" "data-jpa" "lang" "mariadb" "crypto")
 
-lib_dir="${PWD}/jar_files/"
-rm -r ${lib_dir}
-mkdir ${lib_dir}
+pwd="${PWD}/"
+
 cd /$HOME/.m2/repository/org/apache/fineract/cn
 
 echo "Migrating domain microservice"
 for dir in "${domain_ms[@]}"
 do
+    cur_dir="${pwd}/$dir-ms-scripts/"
     cd "$dir/service-boot/0.1.0-BUILD-SNAPSHOT"
-    cp service-boot-0.1.0-BUILD-SNAPSHOT.jar ${lib_dir}$dir-service-boot-0.1.0-BUILD-SNAPSHOT.jar
+    cp service-boot-0.1.0-BUILD-SNAPSHOT.jar ${cur_dir}$dir-service-boot-0.1.0-BUILD-SNAPSHOT.jar
     echo "Completed migrating ${PWD} "
     echo ""
     cd ../../..
 done
-
-echo "----------------------------------------------------------------------------------------"
-echo "Migrating core microservice"
-for dir in "${core_ms[@]}"
-do
-    cd "$dir/0.1.0-BUILD-SNAPSHOT"
-    cp *.jar ${lib_dir}
-    echo "Completed migrating ${PWD} "
-    echo ""
-    cd ../..
-done
-
-cd "anubis/api/0.1.0-BUILD-SNAPSHOT"
-cp *.jar ${lib_dir}
-echo "Completed migrating ${PWD} "
-echo ""
-cd ../..
--- wait_for_db.sh
+++ wait_for_db.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-host="$1"
-port="$2"
-shift
-shift
-cmd="$@"
-
-echo ${host} ${port}
-while ! nc -z ${host} ${port}; do
-    >&2 echo "Cassandra is unavailable - sleeping"
+while ! nc -z 172.16.238.5 9042; do
+  echo "Cassandra is unavailable - sleeping"
   sleep 1
 done
-
->&2 echo "Cassandra is up - executing command"
-exec $cmd
+echo "Cassandra is up - executing command"
