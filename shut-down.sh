b9ada00ebb441aa08c22a1799cf569ec4c813237
Anh3h <courageangeh@gmail.com>
2018-08-03 21:49:55 +0100

    Update database script to start up script and add shut-down script

---
+++ shut-down.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+docker-compose down
+cd external-tools/
+docker-compose down
+cd ..
--- start-up.sh
+++ start-up.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-
+cd external-tools/
+docker-compose up -d
 while ! nc -z 172.16.238.5 9042; do
   echo "Cassandra is unavailable - sleeping"
   sleep 1
 done
 echo "Cassandra is up - executing command"
+cd ..
+docker-compose up -d --build
