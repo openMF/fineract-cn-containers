cde00a0da05e9a394c599a150dc9aceea3964010
Anh3h <courageangeh@gmail.com>
2018-11-10 20:48:39 +0100

    Rename script used to set-up database

--- start-up.sh
+++ start-up.sh
@@ -2,23 +2,23 @@
 
 cd external-tools/
 docker-compose up -d
+  echo "Cassandra is unavailable - sleeping ..."
 while ! nc -z 172.16.238.5 9042; do
-  echo "Cassandra is unavailable - sleeping"
   sleep 1
 done
-echo "Cassandra is up - executing command"
+echo "Cassandra is up - executing command."
 cd ..
 
 cd provisioner-ms-scripts/
-docker build -t provisioner:latest .
+docker build -q -t provisioner:latest .
 docker run -d --name provisioner-ms --env eureka.instance.hostname=172.16.238.2 --env ribbon.listOfServers=172.16.238.2:9090 \
   --env eureka.client.serviceUrl.defaultZone=http://172.16.238.2:8761/eureka --env activemq.brokerUrl=tcp://172.16.238.3:61616 \
   --env cassandra.contactPoints=172.16.238.5:9042 --env mariadb.host=172.16.238.4 --network externaltools_app_net \
   --ip 172.16.238.6 provisioner:latest
 cd ..
 
+echo "Generating RSA keys..."
 while ! docker exec -it provisioner-ms grep -q "provisioner-logger" logs/provisioner.log; do
-  echo "Provisioner is still to generate RSA keys"
   sleep 1
 done
 
